2ba79331099cc40dd2004fc55545c93e2e4ae8e2
VoIyboo <247512367+VoIyboo@users.noreply.github.com>
2026-05-29 10:57:00 +1000

    Update rewrite_authors.sh

--- rewrite_authors.sh
+++ rewrite_authors.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-# Rewrite all authors to VoIyboo
-export GIT_AUTHOR_NAME="VoIyboo"
-export GIT_AUTHOR_EMAIL="amillar566@outlook.com"
-export GIT_COMMITTER_NAME="VoIyboo"
-export GIT_COMMITTER_EMAIL="amillar566@outlook.com"
+
